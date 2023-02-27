8fb0e8101daa3fe99cbe88c2b54441f63d89c88b
Joshua Chen <sidachen2003@gmail.com>
2023-02-26 21:28:20 -0500

    Improve charts placement

--- src/components/IPAChart.tsx
+++ src/components/IPAChart.tsx
@@ -5,21 +5,27 @@ import OtherSymbolsChart from "./charts/OtherSymbolsChart";
 import SuprasegmentalsChart from "./charts/SuprasegmentalsChart";
 import DiacriticsChart from "./charts/DiacriticsChart";
 import TonesChart from "./charts/TonesChart";
+import styles from "./IPAChart.module.css";
+
+const charts = [
+  ConsonantsChart,
+  OtherConsonantsChart,
+  VowelsChart,
+  OtherSymbolsChart,
+  SuprasegmentalsChart,
+  DiacriticsChart,
+  TonesChart,
+];
 
 export default function IPAChart(): JSX.Element {
   return (
-    <div>
-      <ConsonantsChart />
-      <div style={{ display: "flex", flexDirection: "row", gap: "2em" }}>
-        <OtherConsonantsChart />
-        <VowelsChart />
-      </div>
-      <div style={{ display: "flex", flexDirection: "row", gap: "4em" }}>
-        <OtherSymbolsChart />
-        <SuprasegmentalsChart />
-      </div>
-      <DiacriticsChart />
-      <TonesChart />
+    <div className={styles.charts}>
+      {charts.map((Chart, i) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={i} className={styles.chartContainer}>
+          <Chart />
+        </div>
+      ))}
     </div>
   );
 }
